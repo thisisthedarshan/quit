bb0c48a39a1033720ac29a06c0961e12c14a949d
Darshan <techdruler@gmail.com>
2020-07-26 20:25:43 +0530

    Added Feature
    Added PlayStore link and added rate option

--- app/src/main/java/com/aztekstudios/quit/fragments/Share.kt
+++ app/src/main/java/com/aztekstudios/quit/fragments/Share.kt
@@ -1,11 +1,13 @@
 package com.aztekstudios.quit.fragments
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.aztekstudios.quit.R
 
@@ -20,14 +22,16 @@ class Share : Fragment() {
         share.setOnClickListener {
             val shareIntent = Intent(Intent.ACTION_SEND)
             shareIntent.type = "text/plain"
-            shareIntent.putExtra(
-                Intent.EXTRA_SUBJECT,
-                "Hey, check out this cool app. It helped me fight mobile addiction. It could help you too."
-            )
             val shareMessage =
-                "\nHere's the link to Project Quit app.\nhttps://github.com/thisisthedarshan/quit ".trimIndent()
+                ("Hey, check out this cool app. It helped me fight mobile addiction. It could help you too.\nHere's the link to Project Quit.\nhttps://github.com/thisisthedarshan/quit . " +
+                        "The app is available on Play store. Download it here \uD83D\uDC49 https://play.google.com/store/apps/details?com.aztekstudios.quit").trimIndent()
             shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage)
             startActivity(Intent.createChooser(shareIntent, "Share via"))
+        }
+        val rate = root.findViewById<TextView>(R.id.share_support_message)
+        rate.setOnClickListener {
+            val rateIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?com.aztekstudios.quit"))
+            startActivity(rateIntent)
         }
         return root
     }
